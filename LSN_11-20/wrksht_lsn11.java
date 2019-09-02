3f1a5f48721cf62542ca5d6cfe8d2a339c5554ee
the1ridgeway <54417384+the1ridgeway@users.noreply.github.com>
2019-09-02 11:57:20 -0500

    updated files

--- LSN_11-20/wrksht_lsn11.java
+++ LSN_11-20/wrksht_lsn11.java
@@ -1,12 +1,22 @@
+import java.io.*;
+import java.util.*;
 public class wrksht_lsn11
 {
   public static void main(String args[])
   {
-    int n = 24;
-    for(int f = 0;f <= 3; f++)
+    Scanner kb = new Scanner(System.in);
+    System.out.println("Please enter your name._");
+    String name = kb.nextLine();
+    int a, g, len = name.length();
+    for(g = 0;g < len;g++)
     {
-      System.out.println(n);
-      n /= 2;
+      for(a = len;a > 0;a--)
+      {
+        len--;
+        String eman = name.substring(len, a);
+        System.out.print(eman);
+      }
     }
+    System.out.println();
   }
 }
---
+++ LSN_11-20/wrksht_lsn12.java
@@ -0,0 +1,7 @@
+public class wrksht_lsn12
+{
+  public static void main(String args[])
+  {
+    
+  }
+}
